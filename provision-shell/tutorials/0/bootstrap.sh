8e7d700ae56498ec482cb3fb71c5bdd22e2926dc
Liviu Balan <liv_romania@yahoo.com>
2016-01-25 22:18:58 +0200

    Move Provisioning message to the begining of the file, remove Git installation and add 'apt-get update' command

--- provision-shell/tutorials/0/bootstrap.sh
+++ provision-shell/tutorials/0/bootstrap.sh
@@ -1,3 +1,5 @@
+echo 'Provisioning virtual machine...'
+
 # Repair "==> default: stdin: is not a tty" message
 # For more info see http://www.liviubalan.com/vagrant-repair-default-stdin-is-not-a-tty-message
 sudo sed -i '/tty/!s/mesg n/tty -s \\&\\& mesg n/' /root/.profile
@@ -41,11 +43,4 @@ sudo dpkg-reconfigure -f noninteractive tzdata
 # use " > /dev/null 2>&1" in order to redirect stdout to /dev/null
 # For more info see http://stackoverflow.com/questions/10508843/what-is-dev-null-21
 
-echo 'Provisioning virtual machine...'
-
-# Git
-echo 'Installing Git...'
-sudo apt-get -y install git > /dev/null 2>&1
-
-echo 'Removing Git...'
-sudo apt-get -y purge git > /dev/null 2>&1
+sudo apt-get -qq update > /dev/null 2>&1
