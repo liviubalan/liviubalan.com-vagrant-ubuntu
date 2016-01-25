bb28f360214e3cb574e13ccb0dcec815246965cf
Liviu Balan <liv_romania@yahoo.com>
2016-01-25 20:36:25 +0200

    Locale setup: set locale environment variables for current session, compile en_US.UTF-8, do not allow client to pass locale environment variables through SSH, restart SSH service
    For more info see http://www.liviubalan.com/vagrant-repair-setting-locale-failed-message-on-ubuntutrusty32 and http://www.liviubalan.com/ubuntu-server-locales

--- provision-shell/bootstrap.sh
+++ provision-shell/bootstrap.sh
@@ -4,6 +4,35 @@
 # For more info see http://www.liviubalan.com/vagrant-repair-default-stdin-is-not-a-tty-message
 sudo sed -i '/tty/!s/mesg n/tty -s \\&\\& mesg n/' /root/.profile
 
+# Set locale environment variables for current session
+# For more info see http://www.liviubalan.com/vagrant-repair-setting-locale-failed-message-on-ubuntutrusty32
+LANG=en_US.UTF-8
+LANGUAGE=
+LC_CTYPE="en_US.UTF-8"
+LC_NUMERIC="en_US.UTF-8"
+LC_TIME="en_US.UTF-8"
+LC_COLLATE="en_US.UTF-8"
+LC_MONETARY="en_US.UTF-8"
+LC_MESSAGES="en_US.UTF-8"
+LC_PAPER="en_US.UTF-8"
+LC_NAME="en_US.UTF-8"
+LC_ADDRESS="en_US.UTF-8"
+LC_TELEPHONE="en_US.UTF-8"
+LC_MEASUREMENT="en_US.UTF-8"
+LC_IDENTIFICATION="en_US.UTF-8"
+LC_ALL=
+
+# Compile a list of locale definition files
+# For more info see http://www.liviubalan.com/ubuntu-server-locales
+sudo locale-gen en_US.UTF-8
+
+# Do not allow client to pass locale environment variables
+# For more info see http://www.liviubalan.com/vagrant-repair-setting-locale-failed-message-on-ubuntutrusty32
+sudo sed -i "s/^AcceptEnv LANG LC/#AcceptEnv LANG LC/g" /etc/ssh/sshd_config
+
+# Restart SSH service
+sudo service ssh restart
+
 # In order to avoid the message
 # "==> default: dpkg-preconfigure: unable to re-open stdin: No such file or directory"
 # use " > /dev/null 2>&1" in order to redirect stdout to /dev/null
