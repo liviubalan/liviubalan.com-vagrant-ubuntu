91a70e8818bcfc7c3c21a359a5ddec5cd50c2b4c
Liviu Balan <liv_romania@yahoo.com>
2016-01-26 21:16:40 +0200

    Add comments to global variables

--- provision-shell/resources/var.sh
+++ provision-shell/resources/var.sh
@@ -1,19 +1,43 @@
 # Global variables
 
 # Paths
+
+# Resources dir absolute path
 LIV_DIR_RES="$LIV_DIR/resources"
+
+# Tutorials dir absolute path
 LIV_DIR_TUT="$LIV_DIR/tutorials"
 
 # Colors
+
+# Default message color
 LIV_COLOR_DEFAULT='\e[39m'
+
+# Success message color
 LIV_COLOR_SUCCESS='\e[32m'
+
+# Info message color
 LIV_COLOR_INFO='\e[34m'
+
+# Warning message color
 LIV_COLOR_WARNING='\e[33m'
+
+# Danger message color
 LIV_COLOR_DANGER='\e[31m'
 
 # Tutorials
+
+# Maximum tutorial step until the provision will be made
 LIV_TUT_NO=13
+
+# Section provision tutorial message setup
 LIV_TUT_SECTION_NO='Setup for Tutorial '
+
+# Section provision tutorial info message
 LIV_TUT_SECTION_URL='More info on '
+
+# Tutorial provision info shell script
 LIV_TUT_FILE_INFO='info.sh'
+
+# Tutorial provision bootstrap shell script
 LIV_TUT_FILE_BOOT='bootstrap.sh'
