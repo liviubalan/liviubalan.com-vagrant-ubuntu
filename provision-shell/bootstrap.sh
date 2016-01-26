e9f0ad9fd2cde82d7582b9a0c9906385c66fcfb7
Liviu Balan <liv_romania@yahoo.com>
2016-01-26 20:03:38 +0200

    Change tutorial dir structure in order to avoid sorting problems

--- provision-shell/bootstrap.sh
+++ provision-shell/bootstrap.sh
@@ -5,14 +5,19 @@ LIV_DIR="/vagrant/provision-shell"
 source "$LIV_DIR/resources/var.sh"
 source "$LIV_DIR_RES/functions.sh"
 
-for (( i=0; i<=$LIV_TUT_NO; i++ )) do
-    LIV_DIR_SCRIPT_CUR="$LIV_DIR_TUT/$i"
+for (( LIV_I=0; LIV_I<=$LIV_TUT_NO; LIV_I++ )) do
+    if [ "$LIV_I" -eq "0" ]; then
+        LIV_DIR_SCRIPT_CUR=$(liv_tutorial_path 'before')
+    else
+        LIV_DIR_SCRIPT_CUR=$(liv_tutorial_path "$LIV_I")
+    fi
+
     if [ -d "$LIV_DIR_SCRIPT_CUR" ]; then
         # Display data from info script
         LIV_TUT_INFO_PATH="$LIV_DIR_SCRIPT_CUR/$LIV_TUT_FILE_INFO"
         if [ -e "$LIV_TUT_INFO_PATH" ]; then
             source "$LIV_TUT_INFO_PATH"
-            liv_print_section "$i" "$LIV_TUT_INFO_URL"
+            liv_print_section "$LIV_I" "$LIV_TUT_INFO_URL"
         fi
 
         # Load bootstrap script
--- provision-shell/resources/functions.sh
+++ provision-shell/resources/functions.sh
@@ -5,11 +5,12 @@
 # $2 = Color. Default to $LIV_COLOR_DEFAULT
 function liv_print_color {
     if [ "$#" -gt 0 ]; then
-        local COLOR=$LIV_COLOR_DEFAULT
+        local LIV_COLOR_CUR=$LIV_COLOR_DEFAULT
+
         if [ "$#" -gt 1 ]; then
-            COLOR=$2
+            LIV_COLOR_CUR=$2
         fi
-        echo -e "$COLOR$1$LIV_COLOR_DEFAULT"
+        echo -e "$LIV_COLOR_CUR$1$LIV_COLOR_DEFAULT"
     fi
 }
 
@@ -48,4 +49,45 @@ function liv_print_danger {
 function liv_print_section {
     echo -e "$LIV_COLOR_INFO$LIV_TUT_SECTION_NO$LIV_COLOR_DANGER#$1
 $LIV_COLOR_INFO$LIV_TUT_SECTION_URL$LIV_COLOR_DANGER$2$LIV_COLOR_DEFAULT\n"
+}
+
+# Check if a string is integer
+# $1 = Checked string
+function liv_is_int {
+    if [[ $1 =~ ^-?[0-9]+$ ]]; then
+        echo true
+    else
+        echo false
+    fi
+}
+
+# Get the smallest number which is greater than or equal to $1 and is a multiple by $2
+# $1 = Current number
+# $2 = Multiple
+function liv_smallest_multiple {
+    local LIV_I=100
+
+    while [ $1 -gt $LIV_I ]; do
+        LIV_I=$[LIV_I + 100]
+    done
+
+    echo "$LIV_I"
+}
+
+# Get tutorial path
+# $1 = Tutorial number
+function liv_tutorial_path {
+    local LIV_PATH_CUR="$LIV_DIR_TUT"
+    local LIV_TMP_CUR=''
+
+    if [ $(liv_is_int "$1") = true ]; then
+        LIV_TMP_CUR=$(liv_smallest_multiple $1 100)
+        LIV_PATH_CUR="$LIV_PATH_CUR/$LIV_TMP_CUR"
+        LIV_TMP_CUR=$(printf "%03d" $1)
+        LIV_PATH_CUR="$LIV_PATH_CUR/$LIV_TMP_CUR"
+    else
+        LIV_PATH_CUR="$LIV_PATH_CUR/$1"
+    fi
+
+    echo "$LIV_PATH_CUR"
 }
