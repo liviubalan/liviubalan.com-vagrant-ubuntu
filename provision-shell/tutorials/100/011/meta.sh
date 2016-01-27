a59274e25f395f89514eed205f3a53b652302200
Liviu Balan <liv_romania@yahoo.com>
2016-01-27 18:12:47 +0200

    Add provision resources for tutorial http://www.liviubalan.com/install-ubuntu-14-04-lts-server-on-virtualbox

---
+++ provision-shell/tutorials/100/011/meta.sh
@@ -0,0 +1,8 @@
+# (c) Liviu Balan <liv_romania@yahoo.com>
+# http://www.liviubalan.com/
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+
+LIV_TUT_META_URL='http://www.liviubalan.com/install-ubuntu-14-04-lts-server-on-virtualbox'
+LIV_TUT_META_DESCR='This is achieved by using a Vagrant box. For more info see http://www.liviubalan.com/vagrant-boxes-overview'
