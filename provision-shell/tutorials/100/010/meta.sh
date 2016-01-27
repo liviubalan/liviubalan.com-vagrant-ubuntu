b95b2f5505e99c00d85b0fcf6e47d94d33215d46
Liviu Balan <liv_romania@yahoo.com>
2016-01-27 18:02:46 +0200

    Add provision resources for tutorial http://www.liviubalan.com/virtualbox-shared-folders-on-linux-mint

---
+++ provision-shell/tutorials/100/010/meta.sh
@@ -0,0 +1,8 @@
+# (c) Liviu Balan <liv_romania@yahoo.com>
+# http://www.liviubalan.com/
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+
+LIV_TUT_META_URL='http://www.liviubalan.com/virtualbox-shared-folders-on-linux-mint'
+LIV_TUT_META_DESCR='This is achieved by using Vagrant Synced folders. For more info see http://www.liviubalan.com/vagrant-synced-folders'
