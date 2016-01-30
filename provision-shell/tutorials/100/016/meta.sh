4fd8d9da63803f0ba8ad2c6cdde0808b0e7c91b3
Liviu Balan <liv_romania@yahoo.com>
2016-01-30 10:17:02 +0200

    Add provision resources for tutorial http://www.liviubalan.com/install-ssh-on-ubuntu-server

---
+++ provision-shell/tutorials/100/016/meta.sh
@@ -0,0 +1,8 @@
+# (c) Liviu Balan <liv_romania@yahoo.com>
+# http://www.liviubalan.com/
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+
+LIV_TUT_META_URL='http://www.liviubalan.com/install-ssh-on-ubuntu-server'
+LIV_TUT_META_DESCR='This is installed by default on Vagrant. For more info see http://www.liviubalan.com/vagrant-ssh'
