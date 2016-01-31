f3b0854676406152b2e42bc53c93ea2570477319
Liviu Balan <liv_romania@yahoo.com>
2016-01-31 23:10:13 +0200

    Add provision resources for tutorial http://www.liviubalan.com/ssh-client-configuration-on-ubuntu-ssh_config

---
+++ provision-shell/tutorials/100/023/meta.sh
@@ -0,0 +1,8 @@
+# (c) Liviu Balan <liv_romania@yahoo.com>
+# http://www.liviubalan.com/
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+
+LIV_TUT_META_URL='http://www.liviubalan.com/ssh-client-configuration-on-ubuntu-ssh_config'
+LIV_TUT_META_DESCR='For more info see http://www.liviubalan.com/ssh-key-based-authentication-on-ubuntu-server'
