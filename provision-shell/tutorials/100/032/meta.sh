c16d817b042bd71002b3a9db6d041bd2a74af6bd
Liviu Balan <liv_romania@yahoo.com>
2016-02-01 11:14:14 +0200

    Add provision resources for tutorial http://www.liviubalan.com/change-hostname-computer-name-on-ubuntu-server

---
+++ provision-shell/tutorials/100/032/meta.sh
@@ -0,0 +1,9 @@
+# (c) Liviu Balan <liv_romania@yahoo.com>
+# http://www.liviubalan.com/
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+
+LIV_TUT_META_URL='http://www.liviubalan.com/change-hostname-computer-name-on-ubuntu-server'
+LIV_TUT_META_DESCR='In Vagrant, you can change the hostname by using the config.vm.hostname configuration option. '\
+'For more info see http://www.liviubalan.com/vagrant-names'
