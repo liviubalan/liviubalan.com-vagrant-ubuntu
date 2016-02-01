7548458bbd45f29caca8f15b113730d6f634cf12
Liviu Balan <liv_romania@yahoo.com>
2016-02-01 10:55:59 +0200

    Add provision resources for tutorial http://www.liviubalan.com/install-virtualbox-guest-additions-on-ubuntu-server

---
+++ provision-shell/tutorials/100/030/meta.sh
@@ -0,0 +1,8 @@
+# (c) Liviu Balan <liv_romania@yahoo.com>
+# http://www.liviubalan.com/
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+
+LIV_TUT_META_URL='http://www.liviubalan.com/install-virtualbox-guest-additions-on-ubuntu-server'
+LIV_TUT_META_DESCR='Guest Additions installed by default on Vagrant'
