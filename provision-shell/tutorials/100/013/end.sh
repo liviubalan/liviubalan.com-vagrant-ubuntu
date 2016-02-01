32d0d4982dcce14fca3cd180211c2d06a0483db9
Liviu Balan <liv_romania@yahoo.com>
2016-02-01 12:58:39 +0200

    Changes in provision resources for tutorial http://www.liviubalan.com/install-apache-http-web-server-on-ubuntu-server
    Share entire "/var/www/" directory

--- provision-shell/tutorials/100/013/end.sh
+++ provision-shell/tutorials/100/013/end.sh
@@ -6,10 +6,10 @@
 
 sudo apt-get -y install apache2 > /dev/null 2>&1
 
-# Create Apache2 shared directory and point "/var/www/html/" to it.
-if [ -e "/var/www/html/index.html" ]; then
+# Create Apache2 shared directory and point "/var/www/" to it.
+if [ ! -d "$LIV_DIR_SHR_APACHE" ]; then
     mkdir "$LIV_DIR_SHR_APACHE"
-    sudo mv /var/www/html/index.html "$LIV_DIR_SHR_APACHE/"
-    sudo rm -rf /var/www/html/
-    sudo ln -s "$LIV_DIR_SHR_APACHE/" /var/www/html
+    sudo mv /var/www/html/ "$LIV_DIR_SHR_APACHE/"
+    sudo rm -rf /var/www/
+    sudo ln -s "$LIV_DIR_SHR_APACHE" /var/www
 fi
