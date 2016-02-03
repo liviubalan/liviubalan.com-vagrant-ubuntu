75e970f97ac0b3a4d3651907061ac44c4bf6a904
Liviu Balan <liv_romania@yahoo.com>
2016-02-03 16:07:36 +0200

    Changes in resources for tutorial http://www.liviubalan.com/install-mysql-on-ubuntu-server
    Install some other "helper" packages that will assist us in getting our components to communicate with each other

--- provision-shell/tutorials/100/035/end.sh
+++ provision-shell/tutorials/100/035/end.sh
@@ -4,6 +4,7 @@
 # For the full copyright and license information, please view the LICENSE
 # file that was distributed with this source code.
 
+# Install some other "helper" packages that will assist us in getting our components to communicate with each other
 sudo bash -c "echo mysql-server mysql-server/root_password password $LIV_USR_MYSQL_ROOT_PWD | debconf-set-selections"
 sudo bash -c "echo mysql-server mysql-server/root_password_again password $LIV_USR_MYSQL_ROOT_PWD | debconf-set-selections"
-sudo apt-get -y install mysql-server > /dev/null 2>&1
+sudo apt-get -y install mysql-server php5-mysql > /dev/null 2>&1
