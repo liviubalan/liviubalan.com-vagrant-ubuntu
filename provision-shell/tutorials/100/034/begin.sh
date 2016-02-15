c86c232719352fd109665799e8b88ca97d988fc1
Liviu Balan <liv_romania@yahoo.com>
2016-02-15 18:32:41 +0200

    Remove begin.sh for tutorial http://www.liviubalan.com/install-mysql-workbench-on-linux-mintubuntu

--- provision-shell/tutorials/100/034/begin.sh
+++
@@ -1,8 +0,0 @@
-# (c) Liviu Balan <liv_romania@yahoo.com>
-# http://www.liviubalan.com/
-#
-# For the full copyright and license information, please view the LICENSE
-# file that was distributed with this source code.
-
-# Cleanup
-rm -rf /var/www/liviubalan.ro/
