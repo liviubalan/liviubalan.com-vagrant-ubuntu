98ddefab705507fb9625429abbb9845867eacfb3
Liviu Balan <liv_romania@yahoo.com>
2016-02-15 15:51:39 +0200

    Use liv_sed_replace on tutorial http://www.liviubalan.com/install-php-on-ubuntu-server

--- provision-shell/tutorials/100/036/end.sh
+++ provision-shell/tutorials/100/036/end.sh
@@ -8,11 +8,13 @@
 sudo apt-get -y install php5 libapache2-mod-php5 php5-mcrypt > /dev/null 2>&1
 
 # Tell our web server to prefer PHP files
-sudo sed -i 's/DirectoryIndex index.html index.cgi index.pl index.php index.xhtml index.htm/DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm/g' /etc/apache2/mods-enabled/dir.conf
+LIV_TMP_SEARCH=$(cat "$LIV_DIR_SCRIPT_CUR/resources/http.liviubalan.com/apache/dir-search.conf")
+LIV_TMP_REPLACE=$(cat "$LIV_DIR_SCRIPT_CUR/resources/http.liviubalan.com/apache/dir-replace.conf")
+liv_sed_replace "$LIV_TMP_SEARCH" "$LIV_TMP_REPLACE" /etc/apache2/mods-enabled/dir.conf
 
 # Restart Apache to make these changes take effect
 sudo service apache2 restart > /dev/null 2>&1
 
 # Test PHP installation
 rm -rf /var/www/http.liviubalan.com/*
-cp "$LIV_DIR_SCRIPT_CUR/resources/php/http.liviubalan.com/index.php" /var/www/http.liviubalan.com/
+cp "$LIV_DIR_SCRIPT_CUR/resources/http.liviubalan.com/php/index.php" /var/www/http.liviubalan.com/
