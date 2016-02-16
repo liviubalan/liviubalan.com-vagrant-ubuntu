30a725969115434e45414d240639ad861b5e826c
Liviu Balan <liv_romania@yahoo.com>
2016-02-16 16:59:10 +0200

    Add provision resources for tutorial http://www.liviubalan.com/fix-ssh-locale-environment-problems-on-ubuntu-server

--- provision-shell/tutorials/100/040/end.sh
+++ provision-shell/tutorials/100/040/end.sh
@@ -8,7 +8,6 @@ LIV_TMP=$(cat "$LIV_DIR_SCRIPT_CUR/resources/mysql/my.sql")
 
 # For more info see http://unix.stackexchange.com/questions/60688/how-to-defer-variable-expansion
 eval "LIV_TMP=\"$LIV_TMP\""
-echo "$LIV_TMP"
 
 # For more info see http://www.shellhacks.com/en/HowTo-Execute-a-MySQL-Command-from-a-Linux-BASH-Shell
 mysql -u root "-p$LIV_USR_MYSQL_ROOT_PWD" -e "$LIV_TMP" > /dev/null 2>&1
