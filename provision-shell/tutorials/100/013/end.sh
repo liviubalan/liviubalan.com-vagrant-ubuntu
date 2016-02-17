ff64881918319bc51f9cf0688188b723954796ca
Liviu Balan <liv_romania@yahoo.com>
2016-02-17 11:14:17 +0200

    Change http://www.liviubalan.com/install-apache-http-web-server-on-ubuntu-server tutorial in order to avoid vagrant provision problems

--- provision-shell/tutorials/100/013/end.sh
+++ provision-shell/tutorials/100/013/end.sh
@@ -9,7 +9,8 @@ sudo apt-get -y install apache2 > /dev/null 2>&1
 # Create Apache2 shared directory and point "/var/www/" to it
 if [ ! -d "$LIV_DIR_SHR_APACHE" ]; then
     mkdir "$LIV_DIR_SHR_APACHE"
-    sudo mv /var/www/html/ "$LIV_DIR_SHR_APACHE/"
     sudo rm -rf /var/www/
     sudo ln -s "$LIV_DIR_SHR_APACHE" /var/www
+    sudo mkdir /var/www/html
+    sudo cp "$LIV_DIR_SCRIPT_CUR/resources/html/index.html" /var/www/html/
 fi
