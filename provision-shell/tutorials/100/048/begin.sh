345ea613bb0e9a8447cb9ab7ebe12d8beca00515
Liviu Balan <liv_romania@yahoo.com>
2016-02-19 10:10:49 +0200

    Copy files from one directory into another

--- provision-shell/tutorials/100/048/begin.sh
+++ provision-shell/tutorials/100/048/begin.sh
@@ -4,5 +4,6 @@
 # For the full copyright and license information, please view the LICENSE
 # file that was distributed with this source code.
 
-# PHP scripts used
-cp "$LIV_DIR_SCRIPT_CUR/resources/http.liviubalan.com/php/apc_add.php" /var/www/http.liviubalan.com/
+# PHP scripts used.
+# For more info see http://stackoverflow.com/questions/3643848/copy-files-from-one-directory-into-an-existing-directory
+cp -r "$LIV_DIR_SCRIPT_CUR/resources/http.liviubalan.com/php/." /var/www/http.liviubalan.com/
