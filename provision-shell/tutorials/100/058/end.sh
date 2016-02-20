56d07cee561b6257f7ed424aa320814ebeb78cc3
Liviu Balan <liv_romania@yahoo.com>
2016-02-20 19:08:57 +0200

    Update resources for http://www.liviubalan.com/wordpress-permalink-settings
    Move some content from "end.sh" to newly created "begin.sh"

--- provision-shell/tutorials/100/058/end.sh
+++ provision-shell/tutorials/100/058/end.sh
@@ -4,12 +4,6 @@
 # For the full copyright and license information, please view the LICENSE
 # file that was distributed with this source code.
 
-# Set up Apache WP virtual host
-sudo cp "$LIV_DIR_SCRIPT_CUR/resources/wp.liviubalan.com/apache/wp.liviubalan.com.conf" /etc/apache2/sites-available/
-
-# Restart Apache to make these changes take effect
-sudo service apache2 restart > /dev/null 2>&1
-
 cd /var/www/wp.liviubalan.com/
 
 # Update an option
