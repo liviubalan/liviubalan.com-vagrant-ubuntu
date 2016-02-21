d368a3e26ee85f347bcfe5db56b6c5d54d53dbef
Liviu Balan <liv_romania@yahoo.com>
2016-02-21 14:26:22 +0200

    Add provision resources for tutorial http://www.liviubalan.com/vi-show-line-numbers

---
+++ provision-shell/tutorials/100/065/end.sh
@@ -0,0 +1,10 @@
+# (c) Liviu Balan <liv_romania@yahoo.com>
+# http://www.liviubalan.com/
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+
+# Vi Show line numbers.
+# Permanent change.
+# Changes to ".vimrc" are user specific
+cp "$LIV_DIR_SCRIPT_CUR/resources/vagrant/bash/.vimrc"  ~/
