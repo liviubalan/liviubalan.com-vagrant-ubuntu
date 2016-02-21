3ffef0b23bb0c2046e651108c5a57436d8608d29
Liviu Balan <liv_romania@yahoo.com>
2016-02-21 16:47:01 +0200

    Add provision resources for tutorial http://www.liviubalan.com/git-log-command

---
+++ provision-shell/tutorials/100/069/meta.sh
@@ -0,0 +1,7 @@
+# (c) Liviu Balan <liv_romania@yahoo.com>
+# http://www.liviubalan.com/
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+
+LIV_TUT_META_URL='http://www.liviubalan.com/git-log-command'
