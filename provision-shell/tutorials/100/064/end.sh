8dbeea09ee8085a4885b945279778762d2d937a0
Liviu Balan <liv_romania@yahoo.com>
2016-02-21 14:09:55 +0200

    Add provision resources for tutorial http://www.liviubalan.com/wordpress-remove-trailing-slash

---
+++ provision-shell/tutorials/100/064/end.sh
@@ -0,0 +1,10 @@
+# (c) Liviu Balan <liv_romania@yahoo.com>
+# http://www.liviubalan.com/
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+
+cd /var/www/wp.liviubalan.com/
+
+# Update an option
+wp option update 'permalink_structure' '/%postname%' > /dev/null 2>&1
