6c2b0a6e5005eb46f12fac4a0fd1e9811d7776c6
Liviu Balan <liv_romania@yahoo.com>
2016-02-21 20:09:00 +0200

    Add copyright header

--- provision-shell/tutorials/100/038/resources/http.liviubalan.com/php/index.php
+++ provision-shell/tutorials/100/038/resources/http.liviubalan.com/php/index.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * (c) Liviu Balan <liv_romania@yahoo.com>
+ * http://www.liviubalan.com/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * phpinfo
+ * Outputs information about PHP's configuration
+ *
+ * @author Liviu Balan <liv_romania@yahoo.com>
+ * @link http://php.net/manual/en/function.phpinfo.php
+ */
 var_dump(date('c'));
 var_dump(date_default_timezone_get());
 phpinfo();
