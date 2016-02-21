3dcd816af7bd11b92d0b6b6cedfd8f9d649917fe
Liviu Balan <liv_romania@yahoo.com>
2016-02-21 18:00:58 +0200

    Add provision resources for tutorial http://www.liviubalan.com/tree-command-on-ubuntu-server

---
+++ provision-shell/tutorials/100/074/end.sh
@@ -0,0 +1,8 @@
+# (c) Liviu Balan <liv_romania@yahoo.com>
+# http://www.liviubalan.com/
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+
+# Install tree
+sudo apt-get -y install tree > /dev/null 2>&1
